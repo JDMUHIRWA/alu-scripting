e96ec0e77fda38f66851d3c86af54adf04142228
JDMUHIRWA <j.hareriman@alustudent.com>
2024-06-30 17:49:38 +0200

    importing fx

--- api_advanced/0-main.py
+++ api_advanced/0-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Retrieve the number of subscribers from the external module 0-subs.py
-The retrieved subscriber count is then printed to the console
+The retrieved subscriber count is then printed to the console.
 """
 import sys
 
@@ -10,4 +10,11 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        try:
+            subscribers = number_of_subscribers(sys.argv[1])
+            if isinstance(subscribers, int):
+                print("{:d}".format(subscribers))
+            else:
+                print("Error: The function did not return an integer value.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
