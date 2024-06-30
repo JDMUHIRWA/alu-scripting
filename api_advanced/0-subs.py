f14fe3512f76d191b44578dc3fb7f92b91820ee9
JDMUHIRWA <j.hareriman@alustudent.com>
2024-06-30 16:44:51 +0200

    fx returning subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
-"""
-Retrieve the number of subscribers from the external module 0-subs.py
-The retrieved subscriber count is then printed to the console
-"""
+"""Module"""
+
+import json
+import requests
 import sys
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
+
+def number_of_subscribers(subreddit):
+    """Function that return the number of subscribers from REDDIT API"""
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "MyRedditBot/1.0 (by YourUsername)"}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    else:
+        return 0
+
+
+if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        subreddit = sys.argv[1]
+        num_subscribers = number_of_subscribers(subreddit)
+        print(num_subscribers)
