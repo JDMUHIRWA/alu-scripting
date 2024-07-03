df641b9a235529590bf0c2620f0dd2520c977113
JDMUHIRWA <j.hareriman@alustudent.com>
2024-07-03 20:19:30 +0200

    ..

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,16 +1,21 @@
-
 #!/usr/bin/python3
-""" 0-subs module """
+"""get subscribers numbers function"""
+
+
+import json
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """ returns the number of subscribers """
-    url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json().get('data') \
-            .get('subscribers')
-    return 0
+    """get all subscribers"""
+    if len(sys.argv) < 2:
+        return 0
+    else:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        headers = {"User-Agent": "Mozilla/5.0"}
+        result = requests.get(url, headers=headers, allow_redirects=False)
+        if result.status_code != 200:
+            return 0
+        body = json.loads(result.text)
+        return body["data"]["subscribers"]
