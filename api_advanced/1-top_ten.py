9e10783c224b14f8e05d0738f5b5be160cef74cb
JDMUHIRWA <j.hareriman@alustudent.com>
2024-07-09 13:33:33 +0200

    top 10

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,34 +1,25 @@
 
 #!/usr/bin/python3
-"""
-Module
-"""
-
-import json
+'''
+    this module contains the function top_ten
+'''
 import requests
-import sys
+from sys import argv
 
 
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-agent': 'myRedditScript/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'children' in data['data']:
-            top_posts = [post['data']['title']
-                         for post in data['data']['children']]
-            for post_title in top_posts:
-                print(post_title)
-        else:
-            print("No posts found")
-    else:
-        print("None")
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+if __name__ == "__main__":
+    top_ten(argv[1])
